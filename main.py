6528ee1d3fd0fcf68fb8d91d070bdeaa65b54046
JavDomGom <jdomgo3@gmail.com>
2021-06-19 21:26:13 +0200

    Change header size.

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ byte_list = functions.get_bytes(sys.argv[1])
 print(f'{sys.argv[1]}: {byte_list}')
 
 msg_len_bin = bin(len(byte_list))[2:].zfill(config.OP_GET_MSG_BYTES)
-print(f'Msg lenght: {msg_len_bin} ({len(byte_list)})')
+print(f'Msg lenght: {msg_len_bin} ({len(byte_list)} Bytes)')
+
+pixels = int(np.ceil(config.OP_GET_MSG_BYTES/3))
+
+print(f'Voy a usar {pixels} píxeles ({pixels*3} bits) para almacenar OP_GET_MSG_BYTES.')  # noqa: E501
+print(f'Voy a usar {len(byte_list)*config.BITS_X_CHAR} bits para el mensaje.')
 
 url = 'https://www.beatchapter.com/ekmps/shops/beatchapter/images/time-out-magazine-december-17-23-1971-frank-zappa-4-page-interview-a-4-page-island-records-advert-16746-p.jpg'  # noqa: E501
 image_from_internet = Image.open(urllib.request.urlopen(url))
@@ -27,17 +32,18 @@ plt.imshow(image)
 plt.show()
 
 # Veo la forma.
-print(image.shape)  # filas, columnas y profundidad (capas RGB)
+print(f'Dimensiones de la imagen: {image.shape}')  # filas, columnas y capas.
 
-total_pixels = np.prod(image.shape)  # x*y*z
+total_pixels = np.prod(image.shape)  # filas x columnas x capas
+bits_available_to_msg = total_pixels*3-config.OP_GET_MSG_BYTES
+print(f'Bits reservados para la cabecera: {config.OP_GET_MSG_BYTES}')
+print(f'Bits disponibles para el mensaje: {bits_available_to_msg}')
+print(f'Total bits: {total_pixels*3}')
 print(f'Total pixels: {total_pixels}')
+print(f'Longitud máxima del mensaje en imagen: {bits_available_to_msg/8}')
 
 # Ahora veo cómo obtener el valor de un pixel, por ejemplo el primero: 0x0
 print(f'Los 3 colores del pixel son: {image[0, 0]}')
-
-pixels = int(np.ceil(config.OP_GET_MSG_BYTES/3))
-
-print(f'Voy a usar {pixels} píxeles para almacenar OP_GET_MSG_BYTES.')
 
 # Creo filtro para saber qué pixel tiene que ser impar (0) y cuál par (1).
 h_filter = np.array(
--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
 BITS_X_CHAR: int = 8
 
-OP_GET_MSG_BYTES: int = 12  # 12 bits son 4 pixeles de 3 capas (RGB). Max: 4095
+# 24 bits (3 bytes) son 8 pixeles de 3 capas (RGB). Max: 16777215
+OP_GET_MSG_BYTES: int = 24

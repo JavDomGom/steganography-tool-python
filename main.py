51147a7706617240520a577c6144229016690d46
JavDomGom <jdomgo3@gmail.com>
2021-06-20 21:02:38 +0200

    Add some functions and body filter.

--- main.py
+++ main.py
@@ -10,18 +10,19 @@ from src import config, functions
 
 
 byte_list = functions.get_bytes(sys.argv[1])
-
+len_msg = len(byte_list)
 print(f'{sys.argv[1]}: {byte_list}')
 
-msg_len_bin = bin(len(byte_list))[2:].zfill(config.OP_GET_MSG_BYTES)
-print(f'Msg lenght: {msg_len_bin} ({len(byte_list)} Bytes)')
+msg_len_bin = bin(len_msg)[2:].zfill(config.OP_GET_MSG_BYTES)
+print(f'Msg lenght: {msg_len_bin} ({len_msg} Bytes)')
 
 pixels = int(np.ceil(config.OP_GET_MSG_BYTES/3))
 
 print(f'Voy a usar {pixels} píxeles ({pixels*3} bits) para almacenar OP_GET_MSG_BYTES.')  # noqa: E501
-print(f'Voy a usar {len(byte_list)*config.BITS_X_CHAR} bits para el mensaje.')
+print(f'Voy a usar {len_msg*config.BITS_X_CHAR} bits para el mensaje.')
 
-url = 'https://www.beatchapter.com/ekmps/shops/beatchapter/images/time-out-magazine-december-17-23-1971-frank-zappa-4-page-interview-a-4-page-island-records-advert-16746-p.jpg'  # noqa: E501
+# url = 'https://www.beatchapter.com/ekmps/shops/beatchapter/images/time-out-magazine-december-17-23-1971-frank-zappa-4-page-interview-a-4-page-island-records-advert-16746-p.jpg'  # noqa: E501
+url = 'https://d25rq8gxcq0p71.cloudfront.net/dictionary-images/324/f2101e8b-0ae8-4a78-b1f5-4242b6dba0c4.jpg'
 image_from_internet = Image.open(urllib.request.urlopen(url))
 
 # Transformo la imagen en un array de NumPy.
@@ -29,7 +30,7 @@ image = np.array(image_from_internet)
 
 plt.figure(figsize=(10, 20))
 plt.imshow(image)
-plt.show()
+# plt.show()
 
 # Veo la forma.
 print(f'Dimensiones de la imagen: {image.shape}')  # filas, columnas y capas.
@@ -45,22 +46,47 @@ print(f'Longitud máxima del mensaje en imagen: {bits_available_to_msg/8}')
 # Ahora veo cómo obtener el valor de un pixel, por ejemplo el primero: 0x0
 print(f'Los 3 colores del pixel son: {image[0, 0]}')
 
-# Creo filtro para saber qué pixel tiene que ser impar (0) y cuál par (1).
-h_filter = np.array(
-    [list(msg_len_bin[i:i+3]) for i in range(0, len(msg_len_bin), 3)],
-    dtype=int
-)
+h_filter = functions.get_filter(msg_len_bin)
 
 # Imprimo el filtro para ver cómo queda.
 print('h_filter:')
 print(h_filter)
 
 header = image[0, :pixels]
+
 # Imprimo los 4 primeros píxeles a los que voy a aplicar el filtro.
 print('header:')
 print(header)
 
-header[header % 2 == h_filter] -= 1
+functions.apply_filter(header, h_filter)
 
 print('header tras pasarle el filtro:')
 print(header)
+
+pixels_to_msg = int(np.ceil(len_msg*config.BITS_X_CHAR/3))
+print(f'Voy a usar {pixels_to_msg} pixeles para el mensaje.')
+
+bits_string = ''.join(byte_list).ljust(pixels_to_msg*3, '0')
+print(f'bits_string: {bits_string}')
+
+b_filter = functions.get_filter(bits_string)
+
+print('b_filter:')
+print(b_filter)
+
+# -----------------------------------------------------------------------------
+
+body = image[0, pixels:pixels+pixels_to_msg]
+print('body:')
+print(body)
+
+functions.apply_filter(body, b_filter)
+
+print('body tras pasarle el filtro:')
+print(body)
+
+# -----------------------------------------------------------------------------
+
+total_pixels_used = image[0, :pixels+pixels_to_msg]
+print('total_pixels_used:')
+print(total_pixels_used)
--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,36 @@
+import numpy as np
+
 from src import config
 
 
 def get_bytes(msg: str) -> list:
-    return [bin(byte)[2:].zfill(config.BITS_X_CHAR)
-            for byte in bytearray(msg, 'utf-8')]
+    """
+    Returns a list of strings. Each string represents a character as a byte.
+    :param msg: Original message to transform.
+    :return: List of strings.
+    """
+    return [
+        bin(byte)[2:].zfill(config.BITS_X_CHAR)
+        for byte in bytearray(msg, 'utf-8')
+    ]
+
+
+def get_filter(bin_str: str) -> np.ndarray:
+    """
+    Returns a filter to know which pixel has to be odd (0) or even (1).
+    :param bin_str: Binary string.
+    :return: NumPy array of lists of three binary integers (0 or 1).
+    """
+    return np.array(
+        [list(bin_str[i:i+3]) for i in range(0, len(bin_str), 3)],
+        dtype=int
+    )
+
+
+def apply_filter(arr: np.ndarray, arr_filter: np.ndarray) -> None:
+    """
+    Apply a NumPy array to another NumPy array to (+/-)1 to each element.
+    :param bin_str: Original NumPy array with numeric data.
+    :param bin_str: NumPy array fixed by binary filter.
+    """
+    arr[arr % 2 == arr_filter] += 1
